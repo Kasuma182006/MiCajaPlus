73f4ba9b727cb064f243e7b230f6208191ecb531
Kasuma182006 <andresvelasquez182006mail.com>
2026-02-27 12:23:51 -0500

    se agrega la busqueda de los productos en inventario

--- app/src/main/java/com/example/micaja/ConexionService/ConexionServiceTienda.kt
+++ app/src/main/java/com/example/micaja/ConexionService/ConexionServiceTienda.kt
@@ -69,7 +69,7 @@ interface ConexionServiceTienda {
     suspend fun traerInventario(@Query("idTendero") idTendero: String): Response<List<Inventario>>
 
     companion object messi {
-        private const val BASE_URL = "http://10.6.124.193:4000"
+        private const val BASE_URL =  "http://10.6.124.193:4000"
         var inventario = MutableStateFlow<List<Inventario>>(emptyList())
 
         fun create(): ConexionServiceTienda {
--- app/src/main/java/com/example/micaja/fragment_editar_producto.kt
+++ app/src/main/java/com/example/micaja/fragment_editar_producto.kt
@@ -1,11 +1,15 @@
 package com.example.micaja
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.micaja.ConexionService.ConexionServiceTienda
 import com.example.micaja.databinding.ActivityFragmentEditarProductoBinding
+import com.example.micaja.models.Inventario
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class fragment_editar_producto : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,15 +21,59 @@ class fragment_editar_producto : AppCompatActivity() {
         val binding = ActivityFragmentEditarProductoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+
+
         // Botón retroceso
         binding.btnRetroceso.setOnClickListener {
             finish()
         }
+
+
+        // boton Buscar Producto
+        binding.btnBuscarProducto.setOnClickListener {
+            val nombreProducto: String = binding.etBuscarProducto.text.toString().lowercase()
+            val presentacion:String = binding.etBuscarPresentacion.text.toString().lowercase()
+
+            if (nombreProducto != "" && presentacion != ""){
+                buscarProducto(binding, nombreProducto,presentacion)
+            }
+            else {
+                Toast.makeText(this,"Por favor completa los  campos para buscar", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
+
     }
+
+
+    fun buscarProducto(binding: ActivityFragmentEditarProductoBinding,nombreProducto : String, presentacion: String){
+
+        val inventario: MutableStateFlow<List<Inventario>> = ConexionServiceTienda.obtenerInventario()
+
+        for (I in inventario.value ){
+
+            if(I.nombre == nombreProducto && I.presentacion == presentacion){
+                binding.etNombreProducto.setText(I.nombre)
+                binding.etDescripcionProducto.setText(I.presentacion)
+                binding.Proveedor.setText("No hay")
+                binding.etPrecioProducto.setText(I.valorCompra.toString())
+                binding.etCantidadProducto.setText(I.cantidad.toString())
+                break
+            }
+        }
+
+    }
+
+
+
+
 }
